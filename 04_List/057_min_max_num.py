1b441f82acc589dd57c3d1a81ddd750a1898c7d1
Dang-Hyeona <eona1301@gmail.com>
2020-08-16 14:24:17 +0900

    solution date

---
+++ 04_List/057_min_max_num.py
@@ -0,0 +1,4 @@
+nums = [1, 2, 3, 4, 5, 6, 7]
+min_num = min(nums)
+max_num = max(nums)
+print(min_num, max_num)
---
+++ 04_List/060_avg_data.py
@@ -0,0 +1,5 @@
+nums = [1, 2, 3, 4, 5]
+total = 0
+for num in nums :
+    total += num
+print(total/len(nums))
