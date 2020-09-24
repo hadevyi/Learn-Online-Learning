d612f6c8a1e95c088defd4b7aad0ee70929b3ec3
eona1301 <eona1301@gmail.com>
2020-09-24 23:50:54 +0900

    solution update

---
+++ 07_Control/122_score.py
@@ -0,0 +1,12 @@
+score = int(input("score : "))
+
+if 81 <= score <= 100:
+    print("grade is A")
+elif 61 <= score <= 80:
+    print("grade is B")
+elif 41 <= score <= 60:
+    print("grade is C")
+elif 21 <= score <= 40:
+    print("grade is D")
+else:
+    print("grade is E")
