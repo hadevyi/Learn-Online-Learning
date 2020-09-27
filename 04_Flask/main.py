90e001db3dd2996541c7c293b4d332719651d30e
eona1301 <eona1301@gmail.com>
2020-09-27 17:32:15 +0900

    create the web with flask

--- 04_Flask/main.py
+++ 04_Flask/main.py
@@ -1,13 +1,59 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, send_file
+from scrapper import get_jobs
+from exporter import save_to_file
 
-app = Flask("SuperScrapper") # project name
+app = Flask("SuperScrapper")  # project name
+db = {}                       # fake DB
 
-@app.route("/") # someone taping /
-def home() :
-  return "Hello! Welcome to mi sasa!"
 
-@app.route("/contact") # not equal route and function name, @ is see just-under flask
-def contact() :
-  return "Contant me!"
+@app.route("/")  # someone taping /
+def home():
+    #return "Hello! Welcome to mi sasa!"
+    return render_template("home.html")
+
+
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+
+    if word:
+        word = word.lower()  # formatting
+        existingJobs = db.get(word)
+        
+        if existingJobs:
+            jobs = existingJobs
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+    else:
+        return redirect("/")
+    return render_template(
+        "report.html", searchingBy=word, resultsNumber=len(jobs), jobs=jobs)
+
+
+@app.route("/export")
+def export() :
+  try :
+    word = request.args.get('word')
+
+    if not word :
+      raise Exception()    
+    
+    word = word.lower()  # formatting
+    jobs = db.get(word)
+
+    if not jobs :
+      raise Exception()
+    
+    save_to_file(jobs)
+    return send_file("jobs.csv", as_attachment=True)
+  except :
+        return redirect("/")
+'''
+# dynamic URL
+@app.route("/<username>") # not equal route and function name, @ is see just-under flask
+def contact(username) :
+  return F"Hello {username} how are you doing"
+'''
 
 app.run(host="0.0.0.0")
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Python_Web_Scrapping
 Create a job scraper, and start django &#9; | [Nomad coder - python for beginners](https://nomadcoders.co/python-for-beginners)
 
+
+Main Key-word : `python` `beautifulsoup4` `requests` `flask`
+
+
 ### Process
 - prior knowledge : Basic HTML
 - Setting : Chrome
@@ -10,11 +14,12 @@ pip install requests
 pip install beautifulsoup4
 ```
 
-|Chapter|Content||
+|Chapter|Content|Detail|
 |:---:|---|---|
-|01|Basic Python||
-|02|Web Scrapping||
-|03|Django||
+|01|Basic Python|Python Basic Grammar|
+|02|Web Scrapping|Scraping using BeautifulSoup4 <br> - indexed, stack overflow|
+|03|Python Class|OOP, *arg, **kwargs|
+|04|Creating the Web with Flask|render template, html-python|
 
 
 #### Online Compiler : [The collaborative browser based IDE](https://repl.it/)
