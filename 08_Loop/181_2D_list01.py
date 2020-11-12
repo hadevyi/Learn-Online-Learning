83f2ad308c98bd721714fc1b2891902c40531919
eona1301 <eona1301@gmail.com>
2020-11-12 14:08:20 +0900

    solution update

---
+++ 08_Loop/181_2D_list01.py
@@ -0,0 +1,4 @@
+apart = [["101호", "102호"], ["201호", "202호"], ["301호", "302호"]]
+
+for detail in apart:
+    print(detail)
---
+++ 08_Loop/186_2D_print02.py
@@ -0,0 +1,5 @@
+apart = [[101, 102], [201, 202], [301, 302]]
+
+for item in apart[::-1]:
+    for detail in item:
+        print(detail, "호")
