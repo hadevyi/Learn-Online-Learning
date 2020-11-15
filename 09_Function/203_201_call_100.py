886bf26669b51c35716c25d36e2b9b2ecf6e2044
eona1301 <eona1301@gmail.com>
2020-11-15 15:56:31 +0900

    solution upgrade

---
+++ 09_Function/203_201_call_100.py
@@ -0,0 +1,6 @@
+def print_coin():
+    print("비트코인")
+
+
+for _ in range(100):
+    print_coin()
---
+++ 09_Function/205_error_cause.py
@@ -0,0 +1,8 @@
+hello()
+
+
+def hello():
+    print("Hi")
+
+# NameError: name 'hello' is not defined
+# 함수 호출 전에 정의되지 않아 오류가 났다.
