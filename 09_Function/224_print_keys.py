24c2f67a0b03d5b3377c20b6fab32b22bf9a06c6
eona1301 <eona1301@gmail.com>
2020-11-17 14:39:46 +0900

    solution update

---
+++ 09_Function/224_print_keys.py
@@ -0,0 +1,6 @@
+def print_keys(data):
+    for key in data.keys():
+        print(key)
+
+
+print_keys({"이름": "김말똥", "나이": 30, "성별": 0})
---
+++ 09_Function/228_calc_monthly_salary.py
@@ -0,0 +1,5 @@
+def calc_monthly_salary(annual_salary):
+    print(annual_salary//12)
+
+
+calc_monthly_salary(12000000)
