168f590d7e1f4931d2fb42f806f4c4ba1cc3a415
eona1301 <eona1301@gmail.com>
2020-11-21 21:38:29 +0900

    modify detail

--- 11_Class/260_cause_of_error.py
+++ 11_Class/260_cause_of_error.py
@@ -14,5 +14,5 @@ TypeError: print() takes 0 positional arguments but 1 was given
 
 
 이유 : 현재있는 클래스를 통해 print()는 자기자신을 호출하는 재귀함수의 꼴이 된다.
-하지만 정의가 인자가 없는 형식이므로 내부 print에서 "Oh my god"을 정의하면 정의와 호출이 알맞지 않아 argument 오류가 나게된다.
+하지만 Method는 인자가 없는 형식이므로 내부 print에서 "Oh my god"을 정의하면 정의와 호출이 알맞지 않아 argument 오류가 나게된다.
 '''
