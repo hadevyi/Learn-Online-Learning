0aaf63ec611962aedc806b8eee2bf70ad5d591b2
DESKTOP-MEVNCVJ\dang0 <eona1301@gmail.com>
2023-01-17 23:49:32 +0900

    #1 ✨ feat: add jwt token exception about 401, 403

---
+++ Inflearn/Backend/JWT/Tutorial/src/main/java/com/jwt/tutorial/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,18 @@
+package com.jwt.tutorial.jwt;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
